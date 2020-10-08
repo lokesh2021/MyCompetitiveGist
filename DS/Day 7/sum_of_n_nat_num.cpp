615e6b7900d4f0882338f29315e6fb0e7fd97b95
lokesh2021 <lokeshbandi2021@gmail.com>
2020-10-08 21:13:23 +0530

    sum of n natural numbers using recursion

---
+++ DS/Day 7/sum_of_n_nat_num.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Lsum(int n)
+{
+    int s=0;
+    for(int i=1;i<=n;i++){
+        s+=i;
+    }
+    return s;
+}
+
+int Rsum(int n)
+{
+    if(n==0)
+        return 0;
+    return Rsum(n-1)+n;
+
+    
+}
+
+int main()
+{
+    cout<<"Recursive Sum="<<Rsum(5)<<endl;
+    cout<<"Sum Using loop ="<<Lsum(5);
+    return 0;
+}
