624f804b1a37dfcc3a2130a9c11abf7a8c8f4985
lokesh2021 <lokeshbandi2021@gmail.com>
2020-10-28 22:36:19 +0530

    c

---
+++ DS/CodeChefChallenges/Noob_Mathematician.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    char strn[n];
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>strn[i];
+        
+    }
+    cout<<strn;
+    // char *token = strtok(strn,"+");
+    // cout<<token;
+
+}
--- DS/NQTPREP/a.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    cout<<40*35/20-10;
-    cout<<40*35/(20-10);
-}
