cd12b2c01167f53b8228f50ca4f181aa8f497b41
lokesh2021 <lokeshbandi2021@gmail.com>
2020-10-29 21:09:30 +0530

    Todays commit

--- DS/Day 8/taylor.cpp
+++ DS/Day 8/taylor.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 //More the value of X, more accurate the result
+
 //Taylor Series in normal time
 double taylor(int x, int n)
 {
@@ -15,8 +16,38 @@ double taylor(int x, int n)
     return r+p/f;
     
 }
+
+//Taylor Series Horner’s Rule using Recursion
+double horners(int x, int n)
+{
+ static double s;
+ if(n==0)
+ return s;
+ s=1+x*s/n;
+ return horners(x,n-1);
+}
+
+//Taylor Series Horner’s Rule using Iteration
+double hornersi(int x, int n)
+{
+ double s=1;
+ int i;
+ double num=1;
+ double den=1;
+
+ for(i=1;i<=n;i++)
+ {
+ num*=x;
+ den*=i;
+ s+=num/den;
+ }
+ return s;
+}
+
 int main()
 {
-    cout<<taylor(3,10);     //output : 5
+    cout<<"normal: "<<taylor(3,10)<<endl;     //output : 5
+    cout<<"Taylor Series Horner’s Rule using Recursion: "<<horners(3,10)<<endl;     //output : 5
+    cout<<"Taylor Series Horner’s Rule using Iteration: "<<hornersi(3,10)<<endl;     //output : 5
     return 0;
 }
