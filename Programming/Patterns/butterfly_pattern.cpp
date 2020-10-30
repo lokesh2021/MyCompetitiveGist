bdabca0f45f34a64d5ca61a6c3e1b01872bbe305
lokesh2021 <lokeshbandi2021@gmail.com>
2020-10-31 00:00:06 +0530

    butterfly pattern didnt understand

--- Programming/Patterns/butterfly_pattern.cpp
+++ Programming/Patterns/butterfly_pattern.cpp
@@ -1,28 +1,73 @@
-// Butterfly Pattern
+#include <iostream>
 
-#include<iostream>
 using namespace std;
+
 int main()
 {
-    int n=4;
-    // cin>>n;
-    for(int i=1;i<=n;i++)
+	int n, i, j, s;
+    cin>>n;
+    
+	//Left half of the star butterfly start
+    for(i=n ; i>0 ; i--)
     {
-        for(int j=1;j<=n*2;j++)
+        for(j=n ; j>=i ; j--)
         {
-            if(i>=j){
-                cout<<"* ";
-            }
-            else
-                cout<<"  ";
+            cout<<"*";
         }
-        cout<<endl;
+        
+        s = i * 2 - 2;
+        while(s>=0)
+        {
+            cout<<" ";
+            s--;
+        }
+        
+        for(j=i ; j<=n ; j++)
+        {
+            cout<<"*";
+        }
+        
+        cout<<"\n";
     }
-}
+    //Left half of the star butterfly end
+	
+	//Middle line of butterfly start
+    for(i=n ; i>=0 ; i--)
+    {
+        cout<<"*";
+    }
+    
+    for(i=1 ; i<=n ; i++)
+    {
+        cout<<"*";
+    }
+    
+    cout<<"\n";
+	//Middle line of butterfly end
 
-//output:
-// * * * * * 
-// * * * * 
-// * * * 
-// * * 
-// *
+    //Right half of the star butterfly start
+    for(i=1 ; i<=n ; i++)
+    {
+        for(j=n ; j>=i ; j--)
+        {
+            cout<<"*";
+        }
+        
+        s = i * 2 - 2;
+        while(s>=0)
+        {
+            cout<<" ";
+            s--;
+        }
+        
+        for(j=i ; j<=n ; j++)
+        {
+            cout<<"*";
+        }
+        
+        cout<<"\n";
+    }
+	//Right half of the star butterfly end
+
+    return 0;
+}
