56622cc049b65bc39174aae181280172b48bb46e
lokesh2021 <lokeshbandi2021@gmail.com>
2020-11-02 23:21:32 +0530

    armstrong

---
+++ Programming/Prime & Armstrong/armstrong.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n,sum=0;
+    cin>>n;
+    int originaln=n;
+    while(n>0){
+        int lastdigit = n%10;
+        sum+=pow(lastdigit,3);
+        n=n/10;
+    }
+    if(sum==originaln){
+        cout<<"It's an Armstrong Number";
+    }
+    else
+    {
+        cout<<"Not an Armstrong Number ";
+    }
+    
+}
--- Programming/Prime & Armstrong/prime.cpp
+++ Programming/Prime & Armstrong/prime.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main()
 {
     int n;
     cin>>n;
     bool flag=0;
-    for(int i=2;i<10;i++){
+    for(int i=2;i<sqrt(n);i++){
         if(n%i==0){
             cout<<"Not a Prime Number";
             flag=1;
