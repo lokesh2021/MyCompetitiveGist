ac612f23ae642dea490f31ae03a97835dbc4ef7d
lokesh2021 <lokeshbandi2021@gmail.com>
2020-11-02 23:03:09 +0530

    prime numbers

---
+++ Programming/Prime & Armstrong/prime.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    bool flag=0;
+    for(int i=2;i<10;i++){
+        if(n%i==0){
+            cout<<"Not a Prime Number";
+            flag=1;
+            break;
+        } 
+    }
+    if(flag==0){
+        cout<<"Prime Number";
+    }
+}
