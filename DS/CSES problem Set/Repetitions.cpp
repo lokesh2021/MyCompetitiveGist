b1da55f819f8345298f97de6b35bf1116930f356
lokesh2021 <lokeshbandi2021@gmail.com>
2020-11-05 23:41:44 +0530

    commit

--- DS/CSES problem Set/Repetitions.cpp
+++ DS/CSES problem Set/Repetitions.cpp
@@ -5,22 +5,18 @@ int main()
 {
     int a=0,c=0,g=0,t=0;
     string s;
+    vector<int> val;
     cin>>s;
     for(int i=0;i<s.length();i++){
-        if(s[i]=='A')
-            a++;
-        else if (s[i]=='C')
-        {
-            c++;
+        int count = 0;
+        for(int j=i;j<s.length();j++){
+            while(s[j]==s[j+1]){
+                count++;
+j++;
+            }
+            cout<<count<<endl;
         }
-        else if (s[i]=='G')
-        {
-            g++;
-        }
-        else
-        {
-            t++;
-        }
+        
     }
     cout<<a<<" "<<c<<" "<<g<<" "<<t<<" ";
     int ans = max({a,c,g,t});
