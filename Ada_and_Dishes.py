51f937e83f8cea9d2e7867075adb1618063854a6
lokesh2021 <lokeshbandi2021@gmail.com>
2020-11-08 00:16:00 +0530

    commit 5

--- Ada_and_Dishes.py
+++ Ada_and_Dishes.py
@@ -1,19 +1,20 @@
 t = int(input())
 for _ in range(t):
     n = int(input())
-    a = list(map(int,input().split()))
-    a.sort(reverse=True)
+    arr = list(map(int,input().split()))
+    arr.sort(reverse=True)
+    #print(arr)
     if n == 1:
-        print(a[0])
+        print(arr[0])
     else:        
-        num1 = a[0]
-        num2 = a[1]
+        no1 = arr[0]
+        no2 = arr[1]
         for i in range(2,n):
-            if num1<=num2:
-                num1+=a[i]
+            if no1<=no2:
+                no1+=arr[i]
             else:
-                num2+=a[i]
-        if num1>=num2:
-            print(num1)
+                no2+=arr[i]
+        if no1>=no2:
+            print(no1)
         else:
-            print(num2)
+            print(no2)
