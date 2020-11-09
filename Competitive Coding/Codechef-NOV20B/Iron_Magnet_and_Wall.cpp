04663e201946c6078e86fbe750d18c2653fcc78b
lokesh2021 <lokeshbandi2021@gmail.com>
2020-11-09 21:51:48 +0530

    cp commit 3

---
+++ Competitive Coding/Codechef-NOV20B/Iron_Magnet_and_Wall.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+    }
+}
