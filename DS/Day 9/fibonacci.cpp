4a715a62494610c2dd7e34f6f7ea9c86c40a6ebb
lokesh2021 <lokeshbandi2021@gmail.com>
2020-11-10 22:13:05 +0530

    final commit of the day

--- DS/Day 9/fibonacci.cpp
+++ DS/Day 9/fibonacci.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void fib(int n)
 {
     int n1=0,n2=1,n3,i;
-    cout<<"0 1 ";
+    cout<<"Normal Fibonacci: "<<"0 1 ";
     for(i=2;i<n;++i)//loop starts from 2 because 0 and 1 are already printed    
     {    
         n3=n1+n2;    
@@ -12,8 +12,12 @@ void fib(int n)
         n1=n2;    
         n2=n3;    
     }  
-    
+}
 
+int rfib(int n)
+{
+    if(n<=1) return n;
+    return rfib(n-2)+rfib(n-1);
 }
 
 int main()
@@ -22,5 +26,12 @@ int main()
     int n;
     cin>>n;
     fib(n);
+    char c=255;
+    if(c>10){
+        cout<<"g";
+    }
+    else{
+        cout<<"L";
+    }
     return 0;
 }
