8ea47f8179add4d3eaf3d6309e5eac8676381369
lokesh2021 <lokeshbandi2021@gmail.com>
2020-11-14 16:48:32 +0530

    maps in kotlin

--- KotlinIdeaProjects/KotlinLearn/src/Collections.kt
+++ KotlinIdeaProjects/KotlinLearn/src/Collections.kt
@@ -1,9 +1,15 @@
 /*
 Collections in Kotlin are used to store group of related objects in a single unit
+
+   Types of Collections in Kotlin are:-
+
 -> Lists: Ordered Collection with access to elements by indices
+-> Sets: Collection of Unique Elements
+-> Maps: Set of Key-Value Pairs
 */
 
-fun main(args: Array<String>) {
+//Lists
+/*fun main(args: Array<String>) {
     var result = listOf<Int>(1, 3, 3, 4, 5)
     println("Printing elements in List")
     for(i in result)
@@ -19,4 +25,24 @@ fun main(args: Array<String>) {
     println("List after removing element at 2"+numbers)
     numbers.shuffle()
     println("List after Shuffling"+numbers)
+}*/
+
+
+//Sets
+/*fun main(args: Array<String>) {
+    val numbers = setOf(2,4,6,8)
+    println("Number of Elements : ${numbers.size}")
+    if(numbers.contains(3)) println("Element is in the set")
+    else println("No element found")
+
+    val numbers2 = setOf(4,6,8,2)
+    println("Sets are equal: ${numbers==numbers2}")
+}*/
+
+
+//Maps
+fun main(args: Array<String>) {
+    val maps = mapOf<String,Int>("key1" to 2,"key2" to 4,"key3" to 6,"key4" to 8)
+    println("keys: "+maps.keys)
+    println("Values: "+maps.values)
 }
