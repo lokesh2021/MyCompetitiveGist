b3eff856205ae16d652f621ddcbf4a1b21a6df18
lokesh2021 <lokeshbandi2021@gmail.com>
2020-11-18 21:50:59 +0530

    Modifying Arrays using Pointers

--- DS/Day 13 - Arrays/array_using_pointer.cpp
+++ DS/Day 13 - Arrays/array_using_pointer.cpp
@@ -10,20 +10,35 @@ struct Array
 
 void Display(struct Array arr)
 {
-    cout<<"Elements in array are: ";
+    cout<<"Elements in array are: "<<endl;
     for(int i=0;i<arr.length;i++)
     {
         cout<<arr.A[i]<<" ";
     }
 }
 
+void Append(struct Array *arr,int x)
+{
+    if(arr->length<arr->size)
+    {
+        arr->A[arr->length++]=x;
+    }
+
+    cout<<"elements after appending are:"<<endl;
+    for(int i=0;i<arr->length;i++)
+    {
+        cout<<arr->A[i]<<" ";
+    }
+    
+}
+
 int main()
 {
     struct Array arr;
-    cout<<"Enter size of array: ";
+    cout<<"Enter size of array: "<<endl;
     cin>>arr.size;
     arr.A = new int[arr.size];
-    cout<<"Enter the number of elements: ";
+    cout<<"Enter the number of elements: "<<endl;
     cin>>arr.length;
     cout<<"Enter Elements"<<endl;
     for(int i=0;i<arr.length;i++)
@@ -32,6 +47,7 @@ int main()
     }
 
     Display(arr);
+    Append(&arr,9);
     return 0;
 }
 // Output:
