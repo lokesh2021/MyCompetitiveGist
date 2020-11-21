79f59ee47ffc83460599aaf1df03e6c16ca2b75f
lokesh2021 <lokeshbandi2021@gmail.com>
2020-11-21 22:45:32 +0530

    set operation Intersection done

--- DS/Day 14 - Arrays/set_operations.cpp
+++ DS/Day 14 - Arrays/set_operations.cpp
@@ -1,29 +1,103 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<iostream> 
+using namespace std; 
 
-void Union(int a1[], int a2[], int n1, int n2)
-{
-    int i,j,k=0;
-    int n3 = n1+n2;
-    int a3[n3]; 
-    while(i<n1&&j<n2)
-    {
-        if(a1[i]==a2[j])
+int ar3n3 = 0;  //Global array to maintain the 3rd array size ar3n3 = array3 size n3
+
+void Union(int arr1[], int arr2[], int n1, 
+							int n2, int arr3[]) 
+{ 
+	int i = 0, j = 0, k = 0; 
+
+	// Traverse both array 
+	while (i<n1 && j <n2) 
+	{ 
+		// Check if current element of first 
+		// array is smaller than current element 
+		// of second array. If yes, store first 
+		// array element and increment first array 
+		// index. Otherwise do same with second array 
+		if (arr1[i] < arr2[j]) 
+			arr3[k++] = arr1[i++]; 
+		else if(arr2[j] < arr1[i])
+			arr3[k++] = arr2[j++];
+        else
         {
-            a3[i]==a1[i];
-        }
-    }
-    for(int i=0;i<n3;i++)
-    {
-        cout<<i<<" ";
-    }
-}
+            arr3[k++]=arr1[i++];
+            j++;
+        } 
+	} 
 
-int main()
+	// Store remaining elements of first array 
+	while (i < n1) 
+		arr3[k++] = arr1[i++]; 
+
+	// Store remaining elements of second array 
+	while (j < n2) 
+		arr3[k++] = arr2[j++]; 
+
+    ar3n3 = k;
+} 
+
+
+void Intersection(int arr1[], int arr2[], int n1, 
+							int n2, int arr3[]) 
+{ 
+	int i = 0, j = 0, k = 0; 
+
+	// Traverse both array 
+	while (i<n1 && j <n2) 
+	{ 
+		// Check if current element of first 
+		// array is smaller than current element 
+		// of second array. If yes, store first 
+		// array element and increment first array 
+		// index. Otherwise do same with second array 
+		if (arr1[i] < arr2[j]) 
+			i++; 
+		else if(arr2[j] < arr1[i])
+			j++;
+        else if(arr1[i]==arr2[j])
+        {
+            arr3[k++]=arr1[i++];
+            j++;
+        } 
+	} 
+
+    ar3n3 = k;
+} 
+
+
+// Driver code 
+int main() 
 {
-     int arr1[5] = {1,2,3,4,5};
-     int arr2[5] = {6,2,7,4,8};
-    Union(arr1,arr2,5,5);
+
+	int arr1[] = {1, 3, 4, 7}; 
+	int n1 = sizeof(arr1) / sizeof(arr1[0]); 
+
+	int arr2[] = {2, 4, 7, 8}; 
+	int n2 = sizeof(arr2) / sizeof(arr2[0]); 
+
+	int arr3[n1+n2]; 
+	Union(arr1, arr2, n1, n2, arr3); 
+
+	cout << "Array after Union" <<endl; 
+	for (int i=0; i < ar3n3; i++) 
+		cout << arr3[i] << " ";
+
     cout<<endl;
-    cout<<90000*5*73;
-}
+
+    Intersection(arr1, arr2, n1, n2, arr3); 
+	cout << "Array after Intersection" <<endl; 
+	for (int i=0; i < ar3n3; i++) 
+		cout << arr3[i] << " ";
+	return 0;
+
+} 
+
+
+//Output:
+// Array after merging
+// 1 2 3 4 5 7 7 8
+
+// Time Complexity : O(n1 + n2)
+// Auxiliary Space : O(n1 + n2)
