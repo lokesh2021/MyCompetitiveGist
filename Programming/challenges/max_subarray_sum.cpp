aa13d4998b84644e99a918f16622c04a4449eb59
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-11 19:40:24 +0530

    update

--- Programming/challenges/max_subarray_sum.cpp
+++ Programming/challenges/max_subarray_sum.cpp
@@ -2,19 +2,22 @@
 using namespace std;
 int main()
 {
-    int n = 5;
-    int arr[n] = {1, 0, 5, 6, 2};
+    int n = 4;
+    int arr[n] = {-1, 4, 7, 2};
+    int mx = 0;
+
     for (int i = 0; i < n; i++)
     {
-        int sum=0;
+        int sum = 0;
         for (int j = i; j < n; j++)
         {
-            sum+=arr[j];
-            cout<<sum<<endl;
+            sum += arr[j];
+            // cout << sum << endl;
+            mx = max(sum, mx);
         }
-        cout<<endl;
-        
+        // cout << endl;
     }
+    cout << mx;
 }
 
 // output:
