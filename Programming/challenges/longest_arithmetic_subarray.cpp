64f807df22eafe55488809f7ba548ec630aafbe4
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-11 22:45:02 +0530

    Update longest_arithmetic_subarray.cpp

--- Programming/challenges/longest_arithmetic_subarray.cpp
+++ Programming/challenges/longest_arithmetic_subarray.cpp
@@ -1,29 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int n=7;
-    int arr[n] = { 10,7,4,6,8,10,11 };
+    int n = 7;
+    int arr[n] = {10, 7, 4, 6, 8, 10, 11};
 
-    int ans=2;
-    int dif = arr[1]-arr[0];
-    int j=2;
-    int count =2;
-    while(j<n)
+    int ans = 2;
+    int dif = arr[1] - arr[0];
+    int j = 2;
+    int count = 2;
+    while (j < n)
     {
-        if(dif == arr[j]-arr[j-1])
+        if (dif == arr[j] - arr[j - 1])
         {
             count++;
         }
         else
         {
-            dif = arr[j] - arr[j-1];
-            count=2;
+            dif = arr[j] - arr[j - 1];
+            count = 2;
         }
-        ans = max(ans,count);
+        ans = max(ans, count);
         j++;
     }
-    cout<<ans;
+    cout << ans;
 }
 
 // output:
