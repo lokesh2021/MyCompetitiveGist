f79fa2f4dd207d83a43f51be6c3ffa61d4230812
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-11 22:44:45 +0530

    Update max_circular_sub_array_sum.cpp

--- Programming/challenges/max_circular_sub_array_sum.cpp
+++ Programming/challenges/max_circular_sub_array_sum.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int kadane(int arr[],int n)
+int kadane(int arr[], int n)
 {
     int currsum = 0;
     int maxsum = INT_MIN;
@@ -17,21 +17,21 @@ int kadane(int arr[],int n)
 
 int main()
 {
-    int arr[] = {4,-4,6,-6,10,-11,12};
+    int arr[] = {4, -4, 6, -6, 10, -11, 12};
     int n = 7;
     int wrapsum;
     int nonwrapsum;
 
-    nonwrapsum = kadane(arr,n);
+    nonwrapsum = kadane(arr, n);
     int totalsum = 0;
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
         totalsum += arr[i];
-        arr[i]=-arr[i];
+        arr[i] = -arr[i];
     }
-    
-    wrapsum = totalsum + kadane(arr,n);
-    cout<<max(wrapsum,nonwrapsum)<<endl;
+
+    wrapsum = totalsum + kadane(arr, n);
+    cout << max(wrapsum, nonwrapsum) << endl;
 }
 
 // output:
