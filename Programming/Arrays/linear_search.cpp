46cf60ecc6c76bf2d06746f40d94302bab55f2c4
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-11 22:46:17 +0530

    Update linear_search.cpp

--- Programming/Arrays/linear_search.cpp
+++ Programming/Arrays/linear_search.cpp
@@ -1,10 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int LinearSearch(int arr[],int n, int key)
+int LinearSearch(int arr[], int n, int key)
 {
-    for(int i=0;i<5;i++){
-        if(arr[i]==key){
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] == key)
+        {
             return i;
         }
     }
@@ -13,18 +15,19 @@ int LinearSearch(int arr[],int n, int key)
 
 int BinarySearch(int arr[], int n, int key)
 {
-    int s=0,e=n;
-    while(s<=e)
+    int s = 0, e = n;
+    while (s <= e)
     {
-        int mid = (s+e)/2;
-        if(arr[mid]==key) return mid;
-        else if(arr[mid]>key)
+        int mid = (s + e) / 2;
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] > key)
         {
-            e=mid-1;
+            e = mid - 1;
         }
         else
         {
-            s=mid+1;
+            s = mid + 1;
         }
     }
     return -1;
@@ -33,16 +36,15 @@ int BinarySearch(int arr[], int n, int key)
 int main()
 {
     int n;
-    cin>>n;
-    int arr[n],key;
-    for(int i=0;i<n;i++)
+    cin >> n;
+    int arr[n], key;
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    cin>>key;
-    cout<<"Linear Search->"<<endl;
-    cout<<LinearSearch(arr,n,key);
-    cout<<"\nBinary Search->"<<endl;
-    cout<<BinarySearch(arr,n,key);  
-
+    cin >> key;
+    cout << "Linear Search->" << endl;
+    cout << LinearSearch(arr, n, key);
+    cout << "\nBinary Search->" << endl;
+    cout << BinarySearch(arr, n, key);
 }
