051bf156a3e7d6d313a2c87efc2b5dde6bbae870
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-11 21:13:46 +0530

    update

--- Programming/challenges/kadanes.cpp
+++ Programming/challenges/kadanes.cpp
@@ -1,21 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int arr[] ={ -1,4,-6,7,-4 };
+    int arr[] = {-1, 4, -6, 7, -4};
     int n = 5;
     int currsum = 0;
     int maxsum = INT_MIN;
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
         currsum += arr[i];
-        cout<<currsum<<endl;
-        if(currsum<0)
+        cout << currsum << endl;
+        if (currsum < 0)
             currsum = 0;
-        maxsum = max(maxsum,currsum);
+        maxsum = max(maxsum, currsum);
     }
-    cout<<maxsum;
-
+    cout << maxsum;
 }
 
 // output:
--- Programming/challenges/max_subarray_sum.cpp
+++ Programming/challenges/max_subarray_sum.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int n = 4;
-    int arr[n] = {-1,-2,-3,-4};
+    int arr[n] = {-1, -2, -3, -4};
     int mx = -2147483648;
 
     for (int i = 0; i < n; i++)
