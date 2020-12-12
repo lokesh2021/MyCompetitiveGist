dfd9e709439fe983454156d8b7b46aef30d1df42
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-12 18:45:34 +0530

    new new new MERN stack

---
+++ mern/backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 5000;
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI; //database URI to connect to the mongo database
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => { //once the open connection is successfull it prints the below statement
+    console.log("MongoDB database connection established successfully");
+})
+
+const exerciseRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+app.use('/exercises', exerciseRouter);
+app.use('/users', usersRouter);
+
+
+app.listen(port, () => {
+    console.log('server is running on port: ${port}');
+});
