4b10c65105b427e60ee66aae27c06b55e2fbb7f1
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-14 22:14:16 +0530

    react js formatter bug solved

--- mern/mern-exercise-tracker/src/App.js
+++ mern/mern-exercise-tracker/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return ( 
-    <div className = "Container" >
-      Hello World!
-    </div>
+  return (
+    <Router>
+      <div className="Container">Hello World!</div>
+    </Router>
   );
 }
 
