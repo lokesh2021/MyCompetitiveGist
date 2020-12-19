d95694bec3df029bb4be8ed366823e7c8a07a663
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-19 19:39:35 +0530

    update react basics

--- Web-Development/projectreact/src/App.js
+++ Web-Development/projectreact/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import Hello from "./sayHello";
+import Tweet from "./Tweet";
 function App() {
   return (
     <div>
       <h1>Hello React</h1>
+      <Hello />
+      <Tweet/>
     </div>
   );
 }
+
+export default App;
---
+++ Web-Development/projectreact/src/sayHello.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Hello() {
+  const sayHello = () => {
+    console.log("hello");
+  };
+
+  return (
+    <div>
+      <h3>Hello welcome</h3>
+      <button onClick={sayHello}>Say Hello</button>
+    </div>
+  );
+}
+
+export default Hello;
