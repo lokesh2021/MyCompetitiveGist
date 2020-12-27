e15268c48dad8f5b5258c3cd5b7176eaa2bd32ae
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-27 22:05:34 +0530

    file path updated

--- practice.cpp
+++ practice.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
 
 int main()
 {
-    vector<int> arr = {18,19,23,15};
+    vector<int> arr = {18, 19, 23, 15};
     vector<int> arr1 = arr;
     vector<int> sd;
     int n = 4;
-    for(auto i=0;i<n;i++)
+    for (auto i = 0; i < n; i++)
     {
         int s = 0;
-        while(arr[i]!=0)
+        while (arr[i] != 0)
         {
             s = s + arr[i] % 10;
             arr[i] = arr[i] / 10;
@@ -18,22 +18,21 @@ int main()
         sd.push_back(s);
     }
     int mx = 0;
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=i+1;j<n;j++)
+        for (int j = i + 1; j < n; j++)
         {
-            if(sd[i]==sd[j])
+            if (sd[i] == sd[j])
             {
-                int el =arr1[i]+arr1[j]; 
-                mx = max(mx,el);
+                int el = arr1[i] + arr1[j];
+                mx = max(mx, el);
             }
         }
     }
-    if(mx==0)
+    if (mx == 0)
     {
-        cout<<-1;
+        cout << -1;
     }
     else
-        cout<<mx;
-
+        cout << mx;
 }
