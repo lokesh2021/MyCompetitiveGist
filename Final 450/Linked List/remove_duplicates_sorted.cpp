4d2071e2474cc038ea7042fdc61503d3c7efafb5
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-28 22:14:29 +0530

    ll

---
+++ Final 450/Linked List/remove_duplicates_sorted.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+void push(Node **head, int new_data)
+{
+    Node *new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = *head;
+    *head = new_node;
+}
+void printLL(Node *node)
+{
+    while (node != NULL)
+    {
+        cout << "->" << node->data;
+        node = node->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    push(&head, 20);
+    push(&head, 13);
+    push(&head, 13);
+    push(&head, 11);
+    push(&head, 11);
+    push(&head, 11);
+    cout << "Linked list before duplicate removal ";
+    printLL(head);
+}
