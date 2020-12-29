73d14586bd8d58aa381a879f1a03d397df74965b
lokesh2021 <lokeshbandi2021@gmail.com>
2020-12-29 21:42:30 +0530

    cc cp 1 problem solved

---
+++ The_Begining_Era_Of_Cyberverse.cpp
@@ -0,0 +1,21 @@
+//https://www.codechef.com/BYTR20B/problems/CYBV
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    cout << k / n << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
