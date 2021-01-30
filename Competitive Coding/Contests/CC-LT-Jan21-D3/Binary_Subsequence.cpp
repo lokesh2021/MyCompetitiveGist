a872e62cc45b4b80a134838e9f5c14709f7f26a8
Lokesh B M <lokeshbandi2021@gmail.com>
2021-01-30 23:47:31 +0530

    done with todays cp contest

--- Competitive Coding/Contests/CC-LT-Jan21-D3/Binary_Subsequence.cpp
+++ Competitive Coding/Contests/CC-LT-Jan21-D3/Binary_Subsequence.cpp
@@ -1,26 +1,29 @@
+//Didn't solve the problem because there was only 2 mins left for the contest to end
+//https://www.codechef.com/LTIME92C/problems/BINSUBS
 #include <bits/stdc++.h>
 using namespace std;
 
 void solve()
 {
-       int n;
-       cin>>n;
-       vector<int> vec(n);
-       for(int i=0;i<n;i++)
-       {
-           cin>>vec[i];
-       }
-       for(int i=0;i<vec.size();i++)
-       {
-           cout<<vec[i]<<" ";
-       }
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> vec[i];
+    }
+    vec.shrink_to_fit();
+    cout << "\nVector elements are: ";
+    cout<<vec[1];
 }
 
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while(t--){
-    	solve();
+    cin >> t;
+    while (t--)
+    {
+        solve();
     }
     return 0;
-} 
+}
