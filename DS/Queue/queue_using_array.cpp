3545d60bdb689a9950e11e191bdef4a6aa43efa2
Lokesh B M <lokeshbandi2021@gmail.com>
2021-06-30 22:36:06 +0530

    Queue [Done]

--- DS/Queue/queue_using_array.cpp
+++ DS/Queue/queue_using_array.cpp
@@ -1,73 +1,80 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
 
-class Queue
-{
+using namespace std;
+ 
+class Queue{
 private:
     int size;
     int front;
     int rear;
-    int *Q;
-
+    int* Q;
 public:
     Queue(int size);
     ~Queue();
+    bool isFull();
+    bool isEmpty();
     void enqueue(int x);
     int dequeue();
     void display();
-
-    int isEmpty() { return front == rear ? 1 : 0; }
-    int isFull() { return rear == size - 1 ? 1 : 0; }
 };
-
-Queue::Queue(int size)
-{
+ 
+Queue::Queue(int size) {
     this->size = size;
-    front = rear = -1;
-    Q = new int[size];
+    front = -1;
+    rear = -1;
+    Q = new int [size];
 }
-
-Queue::~Queue()
-{
-    delete[] Q;
+ 
+Queue::~Queue() {
+    delete [] Q;
 }
-
-void Queue::enqueue(int x)
-{
-    if (isFull())
-        cout << "Queue is Full"<<endl;
-    else
-    {
-        Q[++rear] = x;
+ 
+bool Queue::isEmpty() {
+    if (front == rear){
+        return true;
+    }
+    return false;
+}
+ 
+bool Queue::isFull() {
+    if (rear == size-1){
+        return true;
+    }
+    return false;
+}
+ 
+void Queue::enqueue(int x) {
+    if (isFull()){
+        cout << "Queue Overflow" << endl;
+    } else {
+        rear++;
+        Q[rear] = x;
     }
 }
-
-int Queue::dequeue()
-{
+ 
+int Queue::dequeue() {
     int x = -1;
-    if (isEmpty())
-        cout << "Queue is Empty";
-    else
-    {
-        x = Q[++front];
+    if (isEmpty()){
+        cout << "Queue Underflow" << endl;
+    } else {
+        front++;
+        x = Q[front];
     }
     return x;
 }
-
-void Queue::display()
-{
-    for (int i = front + 1; i <= rear; i++)
-    {
-        cout<<Q[i]<<flush;
+ 
+void Queue::display() {
+    for (int i=front+1; i<=rear; i++){
+        cout << Q[i] << flush;
         if (i < rear){
             cout << " <- " << flush;
         }
     }
-    cout<<endl;
+    cout << endl;
 }
-
-int main()
-{
+ 
+int main() {
+ 
     int A[] = {1, 3, 5, 7, 9};
  
     Queue q(sizeof(A)/sizeof(A[0]));
@@ -76,16 +83,20 @@ int main()
     for (int i=0; i<sizeof(A)/sizeof(A[0]); i++){
         q.enqueue(A[i]);
     }
-
+ 
     // Display
     q.display();
  
     // Overflow
     q.enqueue(10);
-    
-    //Dequeue
+ 
+    // Dequeue
+    for (int i=0; i<sizeof(A)/sizeof(A[0]); i++){
+        q.dequeue();
+    }
+ 
+    // Underflow
     q.dequeue();
-    q.display();
-
-
+    
+    return 0;
 }
