e25307110d0eb4a5ae099b9bf382377f16419608
Lokesh B M <lokeshbandi2021@gmail.com>
2021-08-25 22:00:55 +0530

    formatted

--- JavaSelfStudy/Wrapper Classes with Auto boxing & unboxing/src/PrimitiveAndObject.java
+++ JavaSelfStudy/Wrapper Classes with Auto boxing & unboxing/src/PrimitiveAndObject.java
@@ -2,18 +2,18 @@ public class PrimitiveAndObject {
     public static void main(String[] args) {
 
         //Primitive to Object and back
-        int a =10;
-        Integer b=Integer.valueOf(a);
-        System.out.println("Integer to Object: "+b);
-        int y= b.intValue();
-        System.out.println("Object to Integer: "+y);
+        int a = 10;
+        Integer b = Integer.valueOf(a);
+        System.out.println("Integer to Object: " + b);
+        int y = b.intValue();
+        System.out.println("Object to Integer: " + y);
 
         //Primitive to String and back
-        byte x =100;
-        String s =Byte.toString(x);
-        System.out.println("Primitive to String: "+s);
+        byte x = 100;
+        String s = Byte.toString(x);
+        System.out.println("Primitive to String: " + s);
         byte z = Byte.parseByte(s);
-        System.out.println("String to Primitive: "+z);
+        System.out.println("String to Primitive: " + z);
 
     }
 }
